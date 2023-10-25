645704400c7f2fa4b670ee3418d64049c75e571c
evtomak <evtomak@iu.edu>
2023-10-25 15:07:56 -0400

    push

--- app/src/main/java/evtomak/iu/edu/notes2/MainActivity.kt
+++ app/src/main/java/evtomak/iu/edu/notes2/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +17,17 @@ class MainActivity : AppCompatActivity() {
         // Initialize Firebase
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
         database = FirebaseDatabase.getInstance().reference
+        auth = FirebaseAuth.getInstance()
+
+        // Check if user is logged in
+        if (auth.currentUser == null) {
+            // User is not logged in, navigate to UserScreen Fragment
+            // TODO: Implement navigation to UserScreen Fragment
+        }
+        else {
+            // User is logged in, show the list of notes
+            // TODO: Implement logic to show notes
+        }
     }
 }
+
