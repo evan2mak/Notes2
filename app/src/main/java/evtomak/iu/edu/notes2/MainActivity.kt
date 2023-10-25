43affcccb83cdbfebfda19bdeb4988cf731a8589
evtomak <evtomak@iu.edu>
2023-10-25 15:02:55 -0400

    push

--- app/src/main/java/evtomak/iu/edu/notes2/MainActivity.kt
+++ app/src/main/java/evtomak/iu/edu/notes2/MainActivity.kt
@@ -1,11 +1,19 @@
 package evtomak.iu.edu.notes2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize Firebase
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
+        database = FirebaseDatabase.getInstance().reference
     }
 }
