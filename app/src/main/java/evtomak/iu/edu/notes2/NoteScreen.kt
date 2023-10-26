bfe2811c2ec1e4a1541ad4fa16a9256fe88fcbdc
evtomak <evtomak@iu.edu>
2023-10-26 14:07:33 -0400

    push

--- app/src/main/java/evtomak/iu/edu/notes2/NoteScreen.kt
+++ app/src/main/java/evtomak/iu/edu/notes2/NoteScreen.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
 
 class NoteScreen : AppCompatActivity() {
     private lateinit var noteViewModel: NoteViewModel
@@ -14,14 +15,20 @@ class NoteScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_screen)
 
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        val toolbar: Toolbar = findViewById(R.id.toolbar1)
         setSupportActionBar(toolbar)
 
+        // Initialize NoteRepository
+        val noteRepository = NoteRepository()
+
         // Initialize ViewModel
-        noteViewModel = NoteViewModelFactory().create(NoteViewModel::class.java)
+        val factory = NoteViewModelFactory(noteRepository)
+        noteViewModel = ViewModelProvider(this, factory).get(NoteViewModel::class.java)
 
         // TODO: Load the note if editing an existing one
     }
+
+    // ... rest of your code
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.note_menu, menu)
