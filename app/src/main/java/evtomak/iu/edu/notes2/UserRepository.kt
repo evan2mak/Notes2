0ea453e1b1afa2d1071dd36a05169fe3af79338e
evtomak <evtomak@iu.edu>
2023-10-26 14:28:11 -0400

    push

--- app/src/main/java/evtomak/iu/edu/notes2/UserRepository.kt
+++ app/src/main/java/evtomak/iu/edu/notes2/UserRepository.kt
@@ -2,26 +2,47 @@ package evtomak.iu.edu.notes2
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
 
 class UserRepository {
+    private val firebaseAuth = FirebaseAuth.getInstance()
     private val _user = MutableLiveData<User?>()
     val user: LiveData<User?> get() = _user
 
-    // TODO: Implement user authentication logic here
+    init {
+        firebaseAuth.currentUser?.let {
+            _user.value = User(it.uid, it.email!!)
+        }
+    }
 
     fun login(email: String, password: String) {
-        // TODO: Implement login logic
-        // Update _user LiveData based on authentication result
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseAuth.currentUser?.let {
+                        _user.value = User(it.uid, it.email!!)
+                    }
+                } else {
+                    // Handle login failure
+                }
+            }
     }
 
     fun register(email: String, password: String) {
-        // TODO: Implement registration logic
-        // Update _user LiveData based on registration result
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseAuth.currentUser?.let {
+                        _user.value = User(it.uid, it.email!!)
+                    }
+                } else {
+                    // Handle registration failure
+                }
+            }
     }
 
     fun logout() {
-        // TODO: Implement logout logic
-        // Clear _user LiveData
+        firebaseAuth.signOut()
         _user.value = null
     }
 }
--- app/src/main/java/evtomak/iu/edu/notes2/UserScreen.kt
+++ app/src/main/java/evtomak/iu/edu/notes2/UserScreen.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 
 class UserScreen : Fragment() {
     private lateinit var userViewModel: UserViewModel
@@ -21,9 +22,15 @@ class UserScreen : Fragment() {
         val factory = UserViewModelFactory(userRepository)
         userViewModel = ViewModelProvider(this, factory).get(UserViewModel::class.java)
 
+        userViewModel.user.observe(viewLifecycleOwner) { user ->
+            if (user != null) {
+                // User is logged in, navigate to notes list
+                findNavController().navigate(R.id.action_userScreen_to_notesListFragment)
+            }
+        }
+
+        // TODO: Implement user authentication UI and logic
+
         return inflater.inflate(R.layout.fragment_user_screen, container, false)
     }
-
-    // TODO: Implement user authentication logic
 }
-
