5c2ccbad06a239bdb0cabcc95d328b81ffe4341f
evtomak <evtomak@iu.edu>
2023-10-29 16:40:05 -0400

    push

--- app/src/main/java/evtomak/iu/edu/notes2/NoteScreen.kt
+++ app/src/main/java/evtomak/iu/edu/notes2/NoteScreen.kt
@@ -6,6 +6,7 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -16,26 +17,36 @@ class NoteScreen : AppCompatActivity() {
     private lateinit var deleteButton: ImageButton
     private val noteViewModel: NoteViewModel by viewModels { NoteViewModelFactory(NoteRepositorySingleton.getInstance()) }
 
+    private var existingNoteId: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note_screen)  // Replace with your actual layout resource ID
+        setContentView(R.layout.activity_note_screen)
 
-        titleEditText = findViewById(R.id.titleEditText)  // Replace with your actual view ID
-        noteEditText = findViewById(R.id.noteEditText)  // Replace with your actual view ID
-        saveButton = findViewById(R.id.saveButton)  // Replace with your actual view ID
-        deleteButton = findViewById(R.id.deleteNoteButton)  // Replace with your actual view ID
+        titleEditText = findViewById(R.id.titleEditText)
+        noteEditText = findViewById(R.id.noteEditText)
+        saveButton = findViewById(R.id.saveButton)
+        deleteButton = findViewById(R.id.deleteNoteButton)
 
-        val noteId = intent.getStringExtra("noteId")
-        if (noteId != null) {
-            loadNoteDetails(noteId)
+        existingNoteId = intent.getStringExtra("noteId")
+        if (existingNoteId != null) {
+            loadNoteDetails(existingNoteId!!)
         }
 
         saveButton.setOnClickListener {
             val title = titleEditText.text.toString()
             val content = noteEditText.text.toString()
             if (title.isNotBlank() && content.isNotBlank()) {
-                val note = Note(UUID.randomUUID().toString(), title, content)
-                noteViewModel.addNote(note)
+                if (existingNoteId == null) {
+                    // Creating a new note
+                    val newNote = Note(UUID.randomUUID().toString(), title, content)
+                    noteViewModel.addNote(newNote)
+                }
+                else {
+                    // Updating an existing note
+                    val updatedNote = Note(existingNoteId!!, title, content)
+                    noteViewModel.updateNote(updatedNote)
+                }
                 finish()
             }
             else {
@@ -44,7 +55,7 @@ class NoteScreen : AppCompatActivity() {
         }
 
         deleteButton.setOnClickListener {
-            deleteNote()
+            showDeleteConfirmationDialog()
         }
     }
 
@@ -53,21 +64,34 @@ class NoteScreen : AppCompatActivity() {
         if (note != null) {
             titleEditText.setText(note.title)
             noteEditText.setText(note.content)
-        } else {
+        }
+        else {
             Toast.makeText(this, "Note not found", Toast.LENGTH_SHORT).show()
             finish()
         }
     }
 
     private fun deleteNote() {
-        val noteId = intent.getStringExtra("NOTE_ID") ?: return
-        val note = noteViewModel.notes.value?.find { it.id == noteId }
-        if (note != null) {
-            noteViewModel.deleteNote(note)
-            finish()
+        existingNoteId?.let { noteId ->
+            val note = noteViewModel.notes.value?.find { it.id == noteId }
+            if (note != null) {
+                noteViewModel.deleteNote(note)
+                finish()
+            }
+            else {
+                Toast.makeText(this, "Note not found", Toast.LENGTH_SHORT).show()
+            }
         }
-        else {
-            Toast.makeText(this, "Note not found", Toast.LENGTH_SHORT).show()
-        }
+    }
+
+    private fun showDeleteConfirmationDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Delete Note")
+            .setMessage("Are you sure you want to delete this note?")
+            .setPositiveButton("Delete") { dialog, which ->
+                deleteNote()
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 }
